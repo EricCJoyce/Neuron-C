471f05ba767802e44f788493b6253c73cf75eeea
EricCJoyce <50152689+EricCJoyce@users.noreply.github.com>
2020-04-09 18:34:37 -0400

    Update xor.c

--- examples/xor.c
+++ examples/xor.c
@@ -25,63 +25,63 @@ int main(int argc, char* argv[])
     linkLayers(INPUT_ARRAY, 0, 0, 2, DENSE_ARRAY, 0, nn);           //  Connect input to dense[0]
     linkLayers(DENSE_ARRAY, 0, 0, 2, DENSE_ARRAY, 1, nn);           //  Connect input to dense[1]
 
-    setW_ij_Dense( 4.169506539262890, 0, 0, nn->denselayers);       //  Set layer 0, unit 0, weight 0
-    setW_ij_Dense( 4.175620772246105, 0, 1, nn->denselayers);       //  Set layer 0, unit 0, weight 1
-    setW_ij_Dense(-6.399885541033798, 0, 2, nn->denselayers);       //  Set layer 0, unit 0, weight 2 (bias)
-    setF_i_Dense(SIGMOID, 0, nn->denselayers);                      //  Set sigmoid function, layer 0, unit 0
+    setW_ij_Dense( 4.169506539262890, 0, 0, nn->denselayers);       //  Set unit 0, weight 0 of layer 0
+    setW_ij_Dense( 4.175620772246105, 0, 1, nn->denselayers);       //  Set unit 0, weight 1 of layer 0
+    setW_ij_Dense(-6.399885541033798, 0, 2, nn->denselayers);       //  Set unit 0, weight 2 (bias) of layer 0
+    setF_i_Dense(SIGMOID, 0, nn->denselayers);                      //  Set activation function of unit 0, layer 0
 
-    setW_ij_Dense( 6.166518349083749, 1, 0, nn->denselayers);       //  Set layer 0, unit 1, weight 0
-    setW_ij_Dense( 6.187965760394095, 1, 1, nn->denselayers);       //  Set layer 0, unit 1, weight 1
-    setW_ij_Dense(-2.678140646720913, 1, 2, nn->denselayers);       //  Set layer 0, unit 1, weight 2 (bias)
-    setF_i_Dense(SIGMOID, 1, nn->denselayers);                      //  Set sigmoid function, layer 0, unit 1
+    setW_ij_Dense( 6.166518349083749, 1, 0, nn->denselayers);       //  Set unit 1, weight 0 of layer 0
+    setW_ij_Dense( 6.187965760394095, 1, 1, nn->denselayers);       //  Set unit 1, weight 1 of layer 0
+    setW_ij_Dense(-2.678140646720913, 1, 2, nn->denselayers);       //  Set unit 1, weight 2 (bias) of layer 0
+    setF_i_Dense(SIGMOID, 1, nn->denselayers);                      //  Set activation function of unit 1, layer 0
 
-    print_Dense(nn->denselayers);
+    print_Dense(nn->denselayers);                                   //  Show me the layer we just built
 
-    setW_ij_Dense(-9.175274710095412, 0, 0, nn->denselayers + 1);   //  Set layer 1, unit 0, weight 0
-    setW_ij_Dense( 8.486130185157748, 0, 1, nn->denselayers + 1);   //  Set layer 1, unit 0, weight 1
-    setW_ij_Dense(-3.875273098510313, 0, 2, nn->denselayers + 1);   //  Set layer 1, unit 0, weight 2 (bias)
-    setF_i_Dense(SIGMOID, 0, nn->denselayers + 1);                  //  Set sigmoid function, layer 1, unit 0
+    setW_ij_Dense(-9.175274710095412, 0, 0, nn->denselayers + 1);   //  Set unit 0, weight 0 of layer 1
+    setW_ij_Dense( 8.486130185157748, 0, 1, nn->denselayers + 1);   //  Set unit 0, weight 1 of layer 1
+    setW_ij_Dense(-3.875273098510313, 0, 2, nn->denselayers + 1);   //  Set unit 0, weight 2 (bias) of layer 1
+    setF_i_Dense(SIGMOID, 0, nn->denselayers + 1);                  //  Set activation function of unit 0, layer 1
 
-    print_Dense(nn->denselayers + 1);
+    print_Dense(nn->denselayers + 1);                               //  Show me the layer we just built
     printf("\n");
     sortEdges(nn);
-    printEdgeList(nn);
-    print_NN(nn);
+    printEdgeList(nn);                                              //  Show me all the connections
+    print_NN(nn);                                                   //  Summarize the network
 
-    write_NN("xor.nn", nn);
+    write_NN("xor.nn", nn);                                         //  Write the network to file
 
-    in[0] = 1.0;
-    in[1] = 0.0;
+    in[0] = 1.0;                                                    //  Now let's try the network out.
+    in[1] = 0.0;                                                    //  This input should produce a signal close to 1.0
     run_NN(in, nn, &out);
     printf("%f %f\n", in[0], in[1]);
     printf("%f\n\n", out[0]);
 
-    in[0] = 1.0;
+    in[0] = 1.0;                                                    //  This input should produce a signal close to 0.0
     in[1] = 1.0;
     run_NN(in, nn, &out);
     printf("%f %f\n", in[0], in[1]);
     printf("%f\n\n", out[0]);
 
-    in[0] = 0.0;
+    in[0] = 0.0;                                                    //  This input should produce a signal close to 0.0
     in[1] = 0.0;
     run_NN(in, nn, &out);
     printf("%f %f\n", in[0], in[1]);
     printf("%f\n\n", out[0]);
 
-    in[0] = 0.0;
+    in[0] = 0.0;                                                    //  This input should produce a signal close to 1.0
     in[1] = 1.0;
     run_NN(in, nn, &out);
     printf("%f %f\n", in[0], in[1]);
     printf("%f\n\n", out[0]);
 
-    free_NN(nn);
+    free_NN(nn);                                                    //  Destroy the network
 
     printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
 
-    init_NN(&nn, 2);
-    load_NN("xor.nn", nn);
+    init_NN(&nn, 2);                                                //  Always initialize before you load
+    load_NN("xor.nn", nn);                                          //  Load the network we just wrote to file
 
-    in[0] = 1.0;
+    in[0] = 1.0;                                                    //  Check that these produce the same outputs
     in[1] = 0.0;
     run_NN(in, nn, &out);
     printf("%f %f\n", in[0], in[1]);
@@ -105,7 +105,6 @@ int main(int argc, char* argv[])
     printf("%f %f\n", in[0], in[1]);
     printf("%f\n\n", out[0]);
 
-    free_NN(nn);
-
-    return 0;
+    free_NN(nn);                                                    //  Clean up
+    return 0;                                                       //  Go home
   }
