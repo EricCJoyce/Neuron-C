7c666fce57dd7c3edc8bfee93ac3679897b76a0d
EricCJoyce <50152689+EricCJoyce@users.noreply.github.com>
2021-07-19 17:03:09 -0400

    Update xor.c

--- examples/xor/xor.c
+++ examples/xor/xor.c
@@ -4,11 +4,6 @@
 cblas_LINUX.a, libblas.a, and cblas.h need to be in the same directory as neuron.h and xor.c.
 This program creates a network file named "xor.nn".
 Issue the following commands to compile and run. 
-
-gcc -c -Wall xor.c
-gfortran -o xor xor.o cblas_LINUX.a libblas.a
-./xor
-
 */
 
 int main(int argc, char* argv[])
